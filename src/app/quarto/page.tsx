20023dc420b8bacf338f5109b8fa4662ef8278c2
Eliot Morin <eliotmorin@MacBook-Air-Eliot.local>
2024-05-29 00:03:05 +0200

    Merci la com

--- src/app/quarto/page.tsx
+++ src/app/quarto/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, DragEvent } from 'react';
 import Image from 'next/image';
 
-const initialPieces = [
-    { id: 'redSquareOpen1', img: '/carré rouge ouvert.jpeg', color: 'red', shape: 'square', type: 'open' },
-    { id: 'redSquareOpen2', img: '/carré rouge ouvert copy.jpeg', color: 'red', shape: 'square', type: 'open' },
-    { id: 'redSquareFull1', img: '/Carré_rouge.svg.png', color: 'red', shape: 'square', type: 'full' },
-    { id: 'redSquareFull2', img: '/Carré_rouge.svg copy.png', color: 'red', shape: 'square', type: 'full' },
-    { id: 'redCircleOpen1', img: '/rond rouge ouvert.png', color: 'red', shape: 'circle', type: 'open' },
-    { id: 'redCircleOpen2', img: '/rond rouge ouvert copy.png', color: 'red', shape: 'circle', type: 'open' },
-    { id: 'redCircleFull1', img: '/rond rouge.png', color: 'red', shape: 'circle', type: 'full' },
-    { id: 'redCircleFull2', img: '/rond rouge copy.png', color: 'red', shape: 'circle', type: 'full' },
-    { id: 'blackSquareOpen1', img: '/Carré noirs ouvert.png', color: 'black', shape: 'square', type: 'open' },
-    { id: 'blackSquareOpen2', img: '/Carré noirs ouvert copy.png', color: 'black', shape: 'square', type: 'open' },
-    { id: 'blackSquareFull1', img: '/carrée noirs.jpeg', color: 'black', shape: 'square', type: 'full' },
-    { id: 'blackSquareFull2', img: '/carrée noirs copy.jpeg', color: 'black', shape: 'square', type: 'full' },
-    { id: 'blackCircleOpen1', img: '/rond noirs ouvert.png', color: 'black', shape: 'circle', type: 'open' },
-    { id: 'blackCircleOpen2', img: '/rond noirs ouvert copy.png', color: 'black', shape: 'circle', type: 'open' },
-    { id: 'blackCircleFull1', img: '/rond noirs.jpeg', color: 'black', shape: 'circle', type: 'full' },
-    { id: 'blackCircleFull2', img: '/rond noirs copy.jpeg', color: 'black', shape: 'circle', type: 'full' }
+const initialPieces: Piece[] = [
+    { id: 'redSquareOpen1', img: '/carré rouge ouvert.jpeg', color: 'red', shape: 'square', type: 'open', size: 'small' },
+    { id: 'redSquareOpen2', img: '/carré rouge ouvert copy.jpeg', color: 'red', shape: 'square', type: 'open', size: 'large' },
+    { id: 'redSquareFull1', img: '/Carré_rouge.svg.png', color: 'red', shape: 'square', type: 'full', size: 'small' },
+    { id: 'redSquareFull2', img: '/Carré_rouge.svg copy.png', color: 'red', shape: 'square', type: 'full', size: 'large' },
+    { id: 'redCircleOpen1', img: '/rond rouge ouvert.png', color: 'red', shape: 'circle', type: 'open', size: 'small' },
+    { id: 'redCircleOpen2', img: '/rond rouge ouvert copy.png', color: 'red', shape: 'circle', type: 'open', size: 'large' },
+    { id: 'redCircleFull1', img: '/rond rouge.png', color: 'red', shape: 'circle', type: 'full', size: 'small' },
+    { id: 'redCircleFull2', img: '/rond rouge copy.png', color: 'red', shape: 'circle', type: 'full', size: 'large' },
+    { id: 'blackSquareOpen1', img: '/Carré noirs ouvert.png', color: 'black', shape: 'square', type: 'open', size: 'small' },
+    { id: 'blackSquareOpen2', img: '/Carré noirs ouvert copy.png', color: 'black', shape: 'square', type: 'open', size: 'large' },
+    { id: 'blackSquareFull1', img: '/carrée noirs.jpeg', color: 'black', shape: 'square', type: 'full', size: 'small' },
+    { id: 'blackSquareFull2', img: '/carrée noirs copy.jpeg', color: 'black', shape: 'square', type: 'full', size: 'large' },
+    { id: 'blackCircleOpen1', img: '/rond noirs ouvert.png', color: 'black', shape: 'circle', type: 'open', size: 'small' },
+    { id: 'blackCircleOpen2', img: '/rond noirs ouvert copy.png', color: 'black', shape: 'circle', type: 'open', size: 'large' },
+    { id: 'blackCircleFull1', img: '/rond noirs.jpeg', color: 'black', shape: 'circle', type: 'full', size: 'small' },
+    { id: 'blackCircleFull2', img: '/rond noirs copy.jpeg', color: 'black', shape: 'circle', type: 'full', size: 'large' }
 ];
 
 type Piece = {
@@ -28,31 +28,28 @@ type Piece = {
     color: string;
     shape: string;
     type: string;
+    size: string;
 };
 
 const Page: React.FC = () => {
     const [boardState, setBoardState] = useState<Array<Piece | null>>(Array(16).fill(null));
     const [currentPlayer, setCurrentPlayer] = useState<number>(1);
-    const [gameMessage, setGameMessage] = useState<string>(`Le joueur 1 joue (Rouge)`);
+    const [gameMessage, setGameMessage] = useState<string>(`Le joueur 1 joue`);
     const [availablePieces, setAvailablePieces] = useState<Piece[]>(initialPieces);
 
     const updateGameInfo = useCallback(() => {
-        setGameMessage(`Le joueur ${currentPlayer} joue (${currentPlayer === 1 ? 'Rouge' : 'Noir'})`);
+        setGameMessage(`Le joueur ${currentPlayer} joue (${currentPlayer === 1 ? '' : ''})`);
     }, [currentPlayer]);
 
     useEffect(() => {
         updateGameInfo();
     }, [updateGameInfo]);
 
-    const handleDragStart = (e: React.DragEvent<HTMLImageElement>, piece: Piece) => {
-        if ((currentPlayer === 1 && piece.color === 'red') || (currentPlayer === 2 && piece.color === 'black')) {
-            e.dataTransfer.setData('text/plain', piece.id);
-        } else {
-            e.preventDefault(); // Prevent the drag if it's not the player's turn
-        }
+    const handleDragStart = (e: DragEvent<HTMLImageElement>, piece: Piece) => {
+        e.dataTransfer.setData('text/plain', piece.id);
     };
 
-    const handleDrop = (e: React.DragEvent<HTMLDivElement>, index: number) => {
+    const handleDrop = (e: DragEvent<HTMLDivElement>, index: number) => {
         const pieceId = e.dataTransfer.getData('text/plain');
         const piece = availablePieces.find(p => p.id === pieceId);
 
@@ -63,7 +60,7 @@ const Page: React.FC = () => {
             setAvailablePieces(availablePieces.filter(p => p.id !== pieceId)); // Remove piece from available pieces
 
             if (checkWin(newBoardState)) {
-                setGameMessage(`Le joueur ${currentPlayer} (${currentPlayer === 1 ? 'Rouge' : 'Noir'}) gagne!`);
+                setGameMessage(`Le joueur ${currentPlayer} (${currentPlayer === 1 ? '' : ''}) gagne!`);
             } else if (newBoardState.every(cell => cell !== null)) {
                 setGameMessage('Match nul!');
             } else {
@@ -82,15 +79,16 @@ const Page: React.FC = () => {
         return winPatterns.some(pattern => {
             const [a, b, c, d] = pattern;
             if (board[a] && board[b] && board[c] && board[d]) {
-                const { color: colorA, shape: shapeA, type: typeA } = board[a]!;
-                const { color: colorB, shape: shapeB, type: typeB } = board[b]!;
-                const { color: colorC, shape: shapeC, type: typeC } = board[c]!;
-                const { color: colorD, shape: shapeD, type: typeD } = board[d]!;
+                const { color: colorA, shape: shapeA, type: typeA, size: sizeA } = board[a]!;
+                const { color: colorB, shape: shapeB, type: typeB, size: sizeB } = board[b]!;
+                const { color: colorC, shape: shapeC, type: typeC, size: sizeC } = board[c]!;
+                const { color: colorD, shape: shapeD, type: typeD, size: sizeD } = board[d]!;
 
                 return (
                     (colorA === colorB && colorA === colorC && colorA === colorD) ||
                     (shapeA === shapeB && shapeA === shapeC && shapeA === shapeD) ||
-                    (typeA === typeB && typeA === typeC && typeA === typeD)
+                    (typeA === typeB && typeA === typeC && typeA === typeD) ||
+                    (sizeA === sizeB && sizeA === sizeC && sizeA === sizeD)
                 );
             }
             return false;
@@ -127,7 +125,7 @@ const Page: React.FC = () => {
                             alt={piece.id}
                             draggable
                             onDragStart={(e) => handleDragStart(e, piece)}
-                            className="w-16 h-16 m-2 cursor-pointer"
+                            className={`w-16 h-16 m-2 cursor-pointer ${piece.size === 'large' ? 'scale-125' : ''}`}
                             width={64}
                             height={64}
                         />
@@ -141,7 +139,7 @@ const Page: React.FC = () => {
                             onDragOver={(e) => e.preventDefault()}
                             onDrop={(e) => handleDrop(e, index)}
                         >
-                            {piece && <Image src={piece.img} alt={piece.id} className="w-16 h-16" width={64} height={64} />}
+                            {piece && <Image src={piece.img} alt={piece.id} className={`w-16 h-16 ${piece.size === 'large' ? 'scale-125' : ''}`} width={64} height={64} />}
                         </div>
                     ))}
                 </div>
@@ -153,7 +151,7 @@ const Page: React.FC = () => {
                             alt={piece.id}
                             draggable
                             onDragStart={(e) => handleDragStart(e, piece)}
-                            className="w-16 h-16 m-2 cursor-pointer"
+                            className={`w-16 h-16 m-2 cursor-pointer ${piece.size === 'large' ? 'scale-125' : ''}`}
                             width={64}
                             height={64}
                         />
