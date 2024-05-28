f2e4144395cedb50b595285f0c52ba8a20918cb6
Victor <victor.masseboeuf@gmail.com>
2024-05-28 20:01:32 +0200

    ad;lkfjads;fkasd;lkfj

--- src/app/userInterface/StepOne.js
+++ src/app/userInterface/StepOne.js
@@ -29,7 +29,7 @@ export default function StepOne({ formData = {}, setFormData, nextStep }) {
         </button>
         <button
           onClick={nextStep}
-          className="mt-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+          className=" mt-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
         >
           Suivant
         </button>
